cc834a997400a7ed17fee5c13cd645bf2756d104
ArturBK <artur.koefender@ilegra.com>
2016-02-09 21:15:22 -0200

    Redirect after POST

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -6,6 +6,10 @@ ProdutosDAO.prototype.lista = function(callback){
 	this._connection.query('select * from livros', callback);
 }
 
+ProdutosDAO.prototype.salva = function(livro,callback) {
+    this._connection.query('INSERT INTO livros SET ?', livro, callback);
+}
+
 module.exports = function() {
 	return ProdutosDAO;
 }
