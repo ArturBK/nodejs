d9b1b0e971020c225b7009de4c112908d2780cbb
ArturBK <artur.koefender@ilegra.com>
2016-02-12 22:13:37 -0200

    mocha testes

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -4,19 +4,43 @@ module.exports = function(app) {
 		var produtosDAO = new app.infra.ProdutosDAO(connection);
 
 		produtosDAO.lista(function(err, results){
-			res.render('produtos/lista', {lista: results});
+			res.format({
+				html: function(){
+					res.render('produtos/lista', {lista: results});
+				},
+				json: function (){
+					res.json(results);
+				}
+			});
 		});
 
 		connection.end();
 	});
 
 	app.get("/produtos/form",function(req, res) {
-		res.render('produtos/form');
+		res.render('produtos/form',{validationErrors:{}, produto:{}});
 	});
 
-	app.post("/produtos/salva",function(req, res) {
+	app.post("/produtos",function(req, res) {
 
 		var produto = req.body;
+
+		req.assert('titulo', 'Título é obrigatório').notEmpty();
+		req.assert('preco', 'formato inválido').isFloat();
+
+		var erros = req.validationErrors();
+
+	        if(errors){
+	              res.format({
+	                html: function(){
+	                    res.status(400).render("produtos/form",{validationErrors:errors,produto:produto});
+	                },
+	                json: function(){
+	                    res.status(400).send(errors);
+	                }
+	            });
+	            return;
+	        }
 
 		var connection = app.infra.connectionFactory();
 		var produtosDAO = new app.infra.ProdutosDAO(connection);
---
+++ cadastra-livros-terminal.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var configuracoes = {
+	hostname: 'localhost',
+	port:3000,
+	path:'/produtos',
+	method: 'post',
+	headers: {
+		'Accept':'application/json',
+		'Content-type':'application/json'
+	}
+};
+
+var client = http.request(configuracoes, function(res){
+	res.on('data', function(body){
+		console.log('corpo' + body);
+	});
+});
+
+var produto = {
+	titulo:'mais sobre node',
+	descricao: 'node, javascript e um pouco de http',
+	preco: 100
+};
+
+client.end(JSON.stringify(produto));
